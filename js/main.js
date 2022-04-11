38aed2f1f4c7d6a5f60b24ee4b1891eea55d1ca5
dd-obua <dd.obua@gmail.com>
2022-04-11 17:22:38 +0300

    Add ids to navigate to the different sections of the page

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const nav = document.querySelector('nav');
+const mobile_icons = document.query('.mobile-icons');
+const hamb = document.querySelector('.humb');
+const close = document.querySelector('.close');
+const options = document.querySelector('.options');
+const projects = document.querySelector('.works_section');
+const about = document.querySelector('.about_section');
+const contact = document.querySelector('.contact_section');
+
+
