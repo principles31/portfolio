49975db83f4990281da3720590c728790163f659
principles31 <principles31@gmail.com>
2022-04-12 16:56:12 +0000

    Add image and update index.html

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const nav = document.querySelector('nav');
-const mobile_icons = document.query('.mobile-icons');
+const mobile_icon = document.query('.mobile-icon');
 const hamb = document.querySelector('.hamb');
 const close_options = document.querySelector('.close');
 const options = document.querySelector('.mobile-options');
@@ -9,19 +9,3 @@ const contact = document.querySelector('.contact_section');
 
 close.style.display = 'none';
 options.style.display = 'none';
-
-const changeOptions = () => {
-    if (nav.classList.contains('click-mobile-icon')) {
-      nav.classList.remove('click-mobile-icon');
-      closeIcon.style.display = 'none';
-      hamburger.style.display = 'block';
-      logo.style.display = 'block';
-      options.style.display = 'none';
-    } else {
-      nav.classList.add('click-mobile-icon');
-      closeIcon.style.display = 'block';
-      hamburger.style.display = 'none';
-      logo.style.display = 'none';
-      options.style.display = 'block';
-    }
-  };
