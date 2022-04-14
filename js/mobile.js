b645fe5ac8adbd1b1d89d33ee8727d3039d73b25
principles31 <principles31@gmail.com>
2022-04-14 19:42:09 +0000

    Add mobile.js file

--- js/mobile.js
+++ js/mobile.js
@@ -1,13 +1,11 @@
-var modal = document.getElementById('myModal');
-
-var btn = document.getElementById('navbar_modal');
-var flex_ul = document.getElementsByClassName('flex_ul')[0];
-var span = document.getElementsByClassName('close')[0];
+let modal = document.getElementById('myModal');
+let btn = document.getElementById('navbar_modal');
+let flex_ul = document.getElementsByClassName('flex_ul')[0];
+let span = document.getElementsByClassName('close')[0];
 
 btn.addEventListener('click', function () {
   modal.style.display = 'block';
   flex_ul.style.display = 'block';
-
   event.preventDefault();
 });
 
@@ -15,9 +13,6 @@ flex_ul.addEventListener("click", function () {
   modal.style.display = "none";
   flex_ul.style.display = "none";
 });
-
-
-
 
 span.onclick = function () {
   modal.style.display = 'none';
