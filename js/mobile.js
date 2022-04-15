533197b4c497164df22a6d020026ebc5e114daff
principles31 <principles31@gmail.com>
2022-04-15 10:22:53 +0000

    Update close button function in js file

--- js/mobile.js
+++ js/mobile.js
@@ -1,11 +1,13 @@
+
 const modal = document.getElementById('myModal');
 const btn = document.getElementById('navbar_modal');
 const flexUl = document.getElementsByClassName('flex_ul')[0];
 const span = document.getElementsByClassName('close')[0];
 
-function showmodal() {
+  function showmodal() {
   modal.style.display = 'block';
   flexUl.style.display = 'block';
+  event.preventDefault()
 }
 
 btn.addEventListener('click', showmodal);
@@ -13,6 +15,7 @@ btn.addEventListener('click', showmodal);
 function hideModal() {
   modal.style.display = 'none';
   flexUl.style.display = 'none';
+  event.preventDefault()
 }
 
 flexUl.addEventListener('click', hideModal);
