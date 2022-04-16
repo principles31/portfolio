78c56ba71accb35ef3a1029ff8d14b640a9ca5ff
principles31 <principles31@gmail.com>
2022-04-16 19:04:30 +0000

    update js file

--- js/mobile.js
+++ js/mobile.js
@@ -26,38 +26,38 @@ span.addEventListener('click', hideModal);
 const projectlist1 = [{
   name: 'Tonic',
   description: 'Take a look of my works. If you like what you see and have a project you need coded',
- featured_image: 'img/work_1.png',
- technologies: ['Html','CSS','Javascript'],
- link_to_source: 'https://github.com/principles31/portfolio/tree/portfolio_popup_window',
- link_to_live: 'https://principles31.github.io/portfolio/'  
+  featured_image: 'img/work_1.png',
+  technologies: ['Html', 'CSS', 'Javascript'],
+  link_to_source: 'https://github.com/principles31/portfolio/tree/portfolio_popup_window',
+  link_to_live: 'https://principles31.github.io/portfolio/'  
 },
 {
-  name:'Multi-Post Stories',
-  description:'Take a look of my works. If you like what you see and have a project you need coded',
-  featured_image:'img/work_2.png',
-  technologies:['Html','CSS','Javascript'],
-  link_to_source:'https://github.com/principles31/portfolio/tree/portfolio_popup_window',
-  link_to_live:'https://principles31.github.io/portfolio/'  
+  name: 'Multi-Post Stories',
+  description: 'Take a look of my works. If you like what you see and have a project you need coded',
+  featured_image: 'img/work_2.png',
+  technologies: ['Html', 'CSS', 'Javascript'],
+  link_to_source: 'https://github.com/principles31/portfolio/tree/portfolio_popup_window',
+  link_to_live: 'https://principles31.github.io/portfolio/'
 },{
   name: 'Tonic',
   description: 'Take a look of my works. If you like what you see and have a project you need coded',
   featured_image: 'img/work_3.png',
-  technologies: ['Html','CSS','Javascript'],
+  technologies: ['Html', 'CSS', 'Javascript'],
   link_to_source: 'https://github.com/principles31/portfolio/tree/portfolio_popup_window',
-  link_to_live: 'https://principles31.github.io/portfolio/' 
+  link_to_live: 'https://principles31.github.io/portfolio/'
 },
 {
   name: 'Multi-Post Stories',
   description: 'Take a look of my works. If you like what you see and have a project you need coded',
   featured_image: 'img/work_4.png',
-  technologies: ['Html','CSS','Javascript'],
+  technologies: ['Html', 'CSS', 'Javascript'],
   link_to_source: 'https://github.com/principles31/portfolio/tree/portfolio_popup_window',
   link_to_live: 'https://principles31.github.io/portfolio/' 
 },{
   name: 'Tonic',
   description: 'Take a look of my works. If you like what you see and have a project you need coded',
   featured_image: 'img/work_5.png',
-  technologies: ['Html','CSS','Javascript'],
+  technologies: ['Html', 'CSS', 'Javascript'],
   link_to_source: 'https://github.com/principles31/portfolio/tree/portfolio_popup_window',
   link_to_live: 'https://principles31.github.io/portfolio/' 
 },
@@ -65,24 +65,20 @@ const projectlist1 = [{
   name: 'Multi-Post Stories',
   description: 'Take a look of my works. If you like what you see and have a project you need coded',
   featured_image: 'img/work_6.png',
-  technologies:['Html','CSS','Javascript'],
+  technologies:['Html', 'CSS', 'Javascript'],
   link_to_source:'https://github.com/principles31/portfolio/tree/portfolio_popup_window',
-  link_to_live:'https://principles31.github.io/portfolio/' 
+  link_to_live:'https://principles31.github.io/portfolio/'
 }]
 
 let count = 0;
 function projectloop(prlist){
-    document.getElementsByClassName('pr')[count].textContent = prlist['name'];
-    document.getElementsByClassName('htm')[count].textContent = prlist['technologies'][0];
-    document.getElementsByClassName('cs')[count].textContent = prlist['technologies'][1];
-    document.getElementsByClassName('jvt')[count].textContent = prlist['technologies'][2];
-    document.getElementsByClassName('g_p')[count].style.backgroundImage = 'url(' + prlist['featured_image'] +')';
-    count += 1
+  document.getElementsByClassName('pr')[count].textContent = prlist['name'];
+  document.getElementsByClassName('htm')[count].textContent = prlist['technologies'][0];
+  document.getElementsByClassName('cs')[count].textContent = prlist['technologies'][1];
+  document.getElementsByClassName('jvt')[count].textContent = prlist['technologies'][2];
+  document.getElementsByClassName('g_p')[count].style.backgroundImage = 'url(' + prlist['featured_image'] +')';
+  count += 1
 }
-
-projectlist1.forEach(projectloop)
-projectLink.forEach(plfunction);
-close2.addEventListener('click', projecthidemodal);
 
 function plfunction(pl) {
   pl.addEventListener('click', projectshowmodal);
@@ -96,8 +92,9 @@ function projecthidemodal(e) {
   projectModal.style.display = 'none';
 }
 
-
-
+projectlist1.forEach(projectloop);
+projectLink.forEach(plfunction);
+close2.addEventListener('click', projecthidemodal);
 
 
 
